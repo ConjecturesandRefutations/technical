3bd73ff03681c48f4efc060074939a231aa81193
Alfie Collins <alfiecollins44@gmail.com>
2023-05-14 17:00:13 +0100

    initial layout of contact page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ function Footer() {
       <div className='Footer'>
         <hr/>
             <p>Website Development by <span style={{ textDecoration: 'underline' }}>AVAMAE</span></p>
+
       </div>
     );
   }
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,12 +1,24 @@
+import ContactImage from './../images/Img_Contact.png'
+
 function ContactUs() {
 
 
     return (
-      <nav className='ContactUs'>
-  
-        <h1>Contact Us</h1>
+
       
-      </nav>
+      <div className='ContactUs'>   
+  <hr id='secondHr'/>
+        <div className="contactText">
+         
+          <h1>Contact Us</h1>
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt.
+          </p>
+        </div>
+        <img src={ContactImage} alt='stylised infinity symbol rotated ninety degrees' id='contactImage'/>
+          
+      
+      </div>
     );
   }
   
